c4fbefb4a5dffaeeeb018d8526323a39b1c9d826
Jonas <j.kuehle@web.de>
2024-03-01 09:27:05 +0000

    cleanup

--- interpret_bipolar.hpp
+++ interpret_bipolar.hpp
@@ -10,30 +10,19 @@ class Bipolar {
 public:
   Bipolar (){};
   template<typename T> Bipolar(T val_) : val(val_) {}
-  Bipolar(Bipolar const& o) : val(o.val) {} // copy constructor
+  Bipolar(Bipolar const& o) : val(o.val) {}
 
   operator int() const {
     return  val? 1 : -1; // convert binary to bipolar for int
   }
-  //Bipolar operator[](int tmp) const { 
-  //  return val;
-  //}
-  // conversion from Bipolar to ap_int
-  //ap_uint<1> operator ap_uint<1>() const { return  val? 1 : 0;}
+
   Bipolar operator*(Bipolar const& o) const {
     return ~(val ^ o.val); // XNOR, bitwise ~ can be used here since it is ap_uint<1> and it is more efficient than "!"
   }
-/*
-  ap_uint<1> operator+(Bipolar const& o) const {
-    return val + o.val; // XNOR, bitwise ~ can be used here since it is ap_uint<1> and it is more efficient than "!"
-  }
-*/
+
 Bipolar operator+=(Bipolar val, Bipolar const& o) const {
     return val = val + o.val; 
   }
 };
 
 #endif
-
-
-
